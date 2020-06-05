03a906183fb8388ba310a6894c6e522db6f19169
Osiel Nicolás <chilparosales@gmail.com>
2020-06-05 08:33:48 -0500

    Operadores aritméticos y de asignación

--- HolaMundo/src/hola_mundo/HolaMundo.java
+++ HolaMundo/src/hola_mundo/HolaMundo.java
@@ -5,23 +5,23 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo \n");
 		
 		//--------------------------------------------------
 		
 		// Tipos de datos primitivos inician con minúscula
 		
 		// Enteros
-		byte edad = 24;  // -128 a 127
+		byte edad = 24;  // Tamaño: -128 a 127
 		short año = 20020;
 		int idUsuario = 1847590;
 		long idAmazon = 109399029304012092L;  // L es necesario al final, para identificarlo como long
 		
 		// Floante (Decimal)
 		float radio = 35.63F;  //F es necesario al final, para identificarlo como float
-		double precio_acumulado = 34542342.546404935945034;
+		double precio_acumulado = 34542342.546404935945034; // Se usa para datos con mayor precisión
 		
-		// Char (De un solo caracter)
+		// Char (Un solo caracter)
 		char genero = 'M';
 		
 		// Lógico (Booleano)
@@ -41,16 +41,14 @@ public class HolaMundo {
 		// Cast automático
 		byte b = 24;
 		short s = b;
-		
-		// Casteo explícito o manual
-		b = (byte) s;
+		b = (byte) s;  // Cast explícito o manual
 		
 		int i = 24;
 		double d = 12.24;
 		i = (int) d;  // Recorta el decimal, solo toma la parte entera del número
 		
 		int codigo = 97;
-		char codigoASCII = (char) codigo;  // Casteo explícito		
+		char codigoASCII = (char) codigo;  // Cast explícito		
 		
 		// Short a Byte
 		short numero = 2020;
@@ -80,15 +78,34 @@ public class HolaMundo {
 		char[][][] days3D = {  { {'a','b','c'}, {'d','e','f'} },
 							   { {'d','e','f'}, {'a','b','c'} }  };
 		
-		// Agregar alementos al arreglo mediante los índices
+		// Agregar elementos al arreglo mediante los índices
 		char[] saludo = new char[4];
+		
 		saludo[0] = 'H';
 		saludo[1] = 'o';
 		saludo[2] = 'l';
 		saludo[3] = 'a';
+		
 		System.out.println(saludo[0]);
 		System.out.println(saludo[1]);
 		System.out.println(saludo[2]);
 		System.out.println(saludo[3]);
+		
+		//-------------------------------------------------
+		
+		// Operadores aritméticos
+		System.out.println(12+21);
+		System.out.println(20-5);
+		System.out.println(12*15);
+		System.out.println(15/3);
+		System.out.println(17%5);
+		
+		// Operadores de asignación
+		int a = 10;
+		System.out.println(a += 2);  // Es equivalente: a = a + 2
+		System.out.println(a -= 3);
+		System.out.println(a *= 4);
+		System.out.println(a /= 5);
+		System.out.println(a %= 6);
 	}
 }
