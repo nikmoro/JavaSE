7be06797be6fcf430c91172d74f0d4d7a10fe4d8
Osiel Nicolás <chilparosales@gmail.com>
2020-06-11 22:42:55 -0500

    Menú del proyecto final

--- MoviesViewer/src/Viewer/Main.java
+++ MoviesViewer/src/Viewer/Main.java
@@ -4,6 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int salir = 0;
+		
+		do {
+			System.out.println("BIENVENIDO A MOVIES VIEWER \n");
+			System.out.println("Selecciona el género deseado:");
+			System.out.println("1. Drama");
+			System.out.println("2. Terror");
+			System.out.println("3. Comedia");
+			System.out.println("4. Acción");
+			System.out.println("0. Salir");
+		} while (salir != 0);
 
 	}
 
