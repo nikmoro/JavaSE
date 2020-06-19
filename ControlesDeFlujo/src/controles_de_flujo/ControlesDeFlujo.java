109ae79c21939f09ca72e8f62b36c689093d0891
Osiel Nicolás <chilparosales@gmail.com>
2020-06-18 23:11:32 -0500

    Agregados for y foreach

--- ControlesDeFlujo/src/controles_de_flujo/ControlesDeFlujo.java
+++ ControlesDeFlujo/src/controles_de_flujo/ControlesDeFlujo.java
@@ -31,17 +31,37 @@ public class ControlesDeFlujo {
 				break;
 		}
 		
-		// Bucle While y do while
+		// While
 		int i = 1;
 		
 		while (i <= 5) {
 			System.out.println("i es menor o igual que 5");
-			i++;  // Control de flujo
+			i++;  // Control del flujo
 		}
 		
+		// Do While
 		do {
 			System.out.println("i es menor o igual que 5");
-			i--;  // Control de flujo
+			i--;  // Control del flujo
 		} while (i >= 5);
+		
+		/* Diferencias:
+		 * for: permite el acceso a los índices
+		 * foreach: es mas rápido y fácil de usar, solo permite el acceso a los datos */
+		
+		// For
+		System.out.println();
+		int[] nums = new int[5];
+		
+		for (int c = 0; c < 5; c++) {
+			nums[c] = c+1;
+			System.out.println("Numeros["+c+"]: " + nums[c]);
+		}
+		
+		// ForEach
+		System.out.println();
+		for (int j : nums) {
+			System.out.println(j);
 		}
 	}
+}
